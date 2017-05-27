d567fda1c45540ad1720c373bf369d551d48abdc
Francine Luise Milian Ribas <francinelmribas@gmail.com>
2017-05-27 11:26:05 -0300

    Inserindo arquivo

--- TCC.jl
+++ TCC.jl
@@ -2,7 +2,7 @@ using CUTEst, BenchmarkProfiles, Plots
 
 include("RegiaoDeConfianca.jl")
 
-problems = CUTEst.select(max_var=2, max_con=0, only_free_var = true)
+problems = CUTEst.select(max_con=0, only_free_var = true)
 
 metodos = [lbfgs_dog, newton_stg]
 
